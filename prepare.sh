6dbb38f634ec1a798bf88db298c0dd2008a8e995
SerenaFeng <serena.feng.711@gmail.com>
2017-08-01 13:06:27 +0000

    install laraval locally

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,15 @@
 # including install composer and dependencies,
 # before run the server
 
+# composer relevant
 curl -s https://getcomposer.org/installer | php
 php composer.phar install
+cp composer.phar /usr/local/bin/composer
+
+# laravel relevant
+composer global require 'laravel/installer'
+export PATH=$HOME/.composer/vendor/bin:$PATH
+
+#install laravel under the current directory
+laravel new
+
